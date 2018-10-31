5347c7f2b9498b2aefc0ae9b8f53ab63648a8eb3
Toru Kobayashi <koba0004@gmail.com>
2018-10-31 10:29:28 +0900

    refactor using Hooks API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Header = styled.h1`
   font-size: 1.5rem;
 `;
 
-class List extends React.PureComponent {
-  render() {
-    return (
-      <ul>
-        {[...new Array(this.props.count)].map((_, i) => (
-          <li key={i}>{this.props.text}</li>
-        ))}
-      </ul>
-    );
-  }
-}
+const Item = React.memo(props => <li>{props.text}</li>);
+
+const List = props => (
+  <ul>
+    {[...new Array(props.count)].map((_, i) => (
+      <Item key={i} text={props.text} />
+    ))}
+  </ul>
+);
 
 const InputText = styled.input.attrs({ type: "text" })`
   height: 1.5rem;
@@ -23,62 +21,52 @@ const InputText = styled.input.attrs({ type: "text" })`
   font-size: 1.2rem;
 `;
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      enabledTimeSlicing: false,
-      repeatCount: 1000,
-      inputText: "",
-      listText: ""
-    };
-  }
-  render() {
-    const { inputText, listText, repeatCount, enabledTimeSlicing } = this.state;
-    return (
-      <section>
-        <Header>TimeSlicing App</Header>
-        <div>
-          <label>
-            <input
-              type="range"
-              min={100}
-              max={10000}
-              step={100}
-              value={repeatCount}
-              onChange={({ target: { value } }) =>
-                this.setState(() => ({ repeatCount: +value }))
-              }
-            />
-            (Repeat Count: {repeatCount})
-          </label>
-          &nbsp;/&nbsp;
-          <label>
-            <input
-              type="checkbox"
-              checked={enabledTimeSlicing}
-              onChange={({ target: { checked } }) =>
-                this.setState(() => ({ enabledTimeSlicing: checked }))
-              }
-            />
-            Enable TimeSlicing
-          </label>
-        </div>
-        <p>Input some text!</p>
-        <InputText
-          value={inputText}
-          onChange={({ target: { value } }) => {
-            this.setState(() => ({ inputText: value }));
-            const wrapper = enabledTimeSlicing
-              ? requestAnimationFrame
-              : cb => cb();
-            wrapper(() => {
-              this.setState(() => ({ listText: value }));
-            });
-          }}
-        />
-        <List text={listText} count={repeatCount} />
-      </section>
-    );
-  }
-}
+const App = () => {
+  const [enabledTimeSlicing, setEnabledTimeSlicing] = useState(false);
+  const [repeatCount, setRepeatCount] = useState(1000);
+  const [inputText, setInputText] = useState("");
+  const [listText, setListText] = useState("");
+
+  const wrapper = enabledTimeSlicing ? requestAnimationFrame : cb => cb();
+
+  return (
+    <section>
+      <Header>TimeSlicing App</Header>
+      <div>
+        <label>
+          <input
+            type="range"
+            min={100}
+            max={10000}
+            step={100}
+            value={repeatCount}
+            onChange={({ target: { value } }) => setRepeatCount(+value)}
+          />
+          (Repeat Count: {repeatCount})
+        </label>
+        &nbsp;/&nbsp;
+        <label>
+          <input
+            type="checkbox"
+            checked={enabledTimeSlicing}
+            onChange={({ target: { checked } }) =>
+              setEnabledTimeSlicing(checked)
+            }
+          />
+          Enable TimeSlicing
+        </label>
+      </div>
+      <p>Input some text!</p>
+      <InputText
+        value={inputText}
+        onChange={({ target: { value } }) => {
+          setInputText(value);
+          wrapper(() => setListText(value));
+        }}
+      />
+      {listText && <List text={listText} count={repeatCount} />}
+    </section>
+  );
+};
+
+export default App;
